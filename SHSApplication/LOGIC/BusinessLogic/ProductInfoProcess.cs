1bd3e7ec4ad03bf721d0279b046e934ed64725cc
Christian <christian.jhardy.steinmann@gmail.com>
2018-05-23 15:56:56 +0200

    Conproduct

--- SHSApplication/LOGIC/BusinessLogic/ProductInfoProcess.cs
+++ SHSApplication/LOGIC/BusinessLogic/ProductInfoProcess.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DATALAYER.Controllers;
+using DATALAYER.DatabaseConnection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,40 @@ namespace LOGIC.BusinessLogic
 {
     public class ProductInfoProcess
     {
+
+        #region ConProduct
+        public static async Task<ConvienceProduct> ConProductInfo(string name)
+        {
+            using (var dbe = new SHSdb())
+            {
+                ConvienceProduct convienceProduct = dbe.convienceProducts.FirstOrDefault((x => x.Name == name));
+                return new ConvienceProduct()
+                {
+                    Name = convienceProduct.Name,
+                    Discription = convienceProduct.Discription,
+                    Price = convienceProduct.Price,
+                    Warrenty = new Warrenty()
+                    {
+                        Discription = convienceProduct.Warrenty.Discription,
+                        Type = convienceProduct.Warrenty.Type,
+                        Duration = convienceProduct.Warrenty.Duration,                        
+                    }
+
+
+                };
+            }
+        }
+        #endregion
+
+
+
+
+            #region SafeProduct
+
+            #endregion
+            #region EnergyProduct
+
+            #endregion
+
     }
 }
