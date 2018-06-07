41994fec7f5639fb6c50d50e2f92556b95e11436
Christian <christian.jhardy.steinmann@gmail.com>
2018-06-07 10:43:47 +0200

    vdcvbcv

---
+++ SHSApplication/LOGIC/BusinessLogic/UserDelete.cs
@@ -0,0 +1,24 @@
+﻿using DATALAYER.Controllers;
+using DATALAYER.DatabaseConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOGIC.BusinessLogic
+{
+    public static class UserDelete
+    {
+        public static async Task Delete(People person)
+        {
+            using (var db = new SHSdb())
+            {
+                People existing =  db.peoples.FirstOrDefault(x => x.EmailAddress == person.EmailAddress);
+                if (existing == null) { throw new KeyNotFoundException(); }
+                db.peoples.DeleteOnSubmit(existing);
+                db.SubmitChanges();
+            }
+        }
+    }
+}
